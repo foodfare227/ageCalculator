2a10c4ee82a6c4be049427d8cc1cce1c96eb09f7
foodfare227 <105768994+foodfare227@users.noreply.github.com>
2022-05-18 01:01:09 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function ageCalculator() {
+    var userinput = document.getElementById("DOB").value;
+    var dob = new Date(userinput);
+    if (userinput == null || userinput == '') {
+        document.getElementById("message").innerHTML = "**Choose a date please!";
+        return false;
+    } else {
+        var month_diff = Date.now() - dob.getTime();
+        var age_dt = new Date(month_diff);
+        var year = age_dt.getUTCFullYear();
+        var age = Math.abs(year - 1970);
+        if (age >= 21) {
+            result.style.color = "green";
+        } else {
+            result.style.color = "red";
+        }
+        return document.getElementById("result").innerHTML =
+            "Age is: " + age;
+    }
+}
